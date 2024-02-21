093dd06d9d987aec9bf4d29974a68d6baa3c88ce
Antoine Romand <antoine.romand@epitech.eu>
2024-02-21 13:58:06 +0100

    add in user repository impl userExists method

--- api_authentication/src/main/java/com/antoineromand/api_authentication/infrastructure/repository/UserRepositoryImpl.java
+++ api_authentication/src/main/java/com/antoineromand/api_authentication/infrastructure/repository/UserRepositoryImpl.java
@@ -1,12 +1,18 @@
 package com.antoineromand.api_authentication.infrastructure.repository;
 
-import com.antoineromand.api_authentication.infrastructure.repository.interfaces.UserRepository;
+import com.antoineromand.api_authentication.domain.contract.UserRepository;
 
 public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void saveUser() {
         System.out.println("User saved");
+    }
+
+    @Override
+    public boolean userExists(String email) {
+        System.out.println(email);
+        return true;
     }
 
 }
